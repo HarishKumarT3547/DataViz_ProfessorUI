08d654520535f2d34e4636b8fc02c7a2a7b6136b
ai-dani <airadani25@gmail.com>
2023-10-04 21:01:59 -0700

    implement POST in frontend (right after upload)

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 import { FileUploadService } from './file-upload.service';
 import { SnackbarService } from '../snackbar/snackbar.service';
+import { PythonModelService } from '../python-model/python-model.service';
 import { FileUpload } from '../models/file-upload';
-import { AngularFireList } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-file-upload',
@@ -13,12 +11,12 @@ import { AngularFireList } from '@angular/fire/compat/database';
 })
 export class FileUploadComponent {
 
-  // Declaring variables
   currentFile?: File;
   progress = 0;
   fileName = 'Select File';
+  imageUrl: any; // for preview
 
-  constructor(private fileUploadService: FileUploadService, private snackBarService: SnackbarService) { }
+  constructor(private fileUploadService: FileUploadService, private snackBarService: SnackbarService, private pythonModelService: PythonModelService) { }
 
   // Getting file details and dividing into variables
   selectFile(event: any): void {
@@ -35,6 +33,22 @@ export class FileUploadComponent {
   // Uploading file to firebase
   upload(): void {
     if (this.currentFile) {
+
+        // 1. generate preview of point cloud model using PYTHON service
+        this.pythonModelService.generateModelPreview(this.currentFile).subscribe(
+          data => {
+            try {
+              this.imageUrl = URL.createObjectURL(data);
+            } catch (error) {
+              console.error("Failed to create image:", error);
+            }
+          },
+          error => {
+            console.error("HTTP Request Error:", error);
+          }
+        );
+
+        // 2. after preview generation, pass in the file and preview thumbnail to FIREBASE
         const currentFileUpload = new FileUpload(this.currentFile);
         this.fileUploadService.uploadFile(currentFileUpload).subscribe(
           percentage => {
--- src/app/python-model/python-model.service.ts
+++ src/app/python-model/python-model.service.ts
@@ -1,22 +1,26 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class PythonModelService {
   private apiUrl = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   // simple hello world from python
   getMessage(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/hello`);
   }
 
-  generateImage(): Observable<Blob> {
-    return this.http.get(`${this.apiUrl}/generate_image`, { responseType: 'blob' });
+  // returns image response if file upload has been successful
+  generateModelPreview(uploadedFile: File): Observable<Blob> {
+    const formData = new FormData();
+    formData.append('file', uploadedFile);
+    return this.http.post(`${this.apiUrl}/upload`, formData, {
+      responseType: 'blob',
+    });
   }
 }
