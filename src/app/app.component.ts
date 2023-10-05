44acdfcbcb4749c39356109a2fd800cf963413da
ai-dani <airadani25@gmail.com>
2023-10-04 20:56:27 -0700

    clean up app component scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PythonModelService } from './python-model/python-model.service';
 
 @Component({
   selector: 'app-root',
@@ -8,30 +7,4 @@ import { PythonModelService } from './python-model/python-model.service';
 })
 export class AppComponent {
   title = 'ProfessorUI';
-  message: string = '';
-
-// TODO: THIS DIV BLOCK IS JUST A TEST FOR PYTHON CONNECTION. MIGRATE THIS TO PREVIEW PAGE AFTER.
-  constructor(private pythonModelService: PythonModelService) {}
-  getMessage() {
-    this.pythonModelService.getMessage().subscribe(data => {
-      console.log(data.message)
-      this.message = data.message;
-    });
-  }
-
-  imageUrl: any;
-  generateImage() {
-    this.pythonModelService.generateImage().subscribe(
-      data => {
-        try {
-          this.imageUrl = URL.createObjectURL(data);
-        } catch (error) {
-          console.error("Failed to create image:", error);
-        }
-      },
-      error => {
-        console.error("HTTP Request Error:", error);
-      }
-    );
-  }
 }
